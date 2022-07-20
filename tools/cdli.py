96616d87cadefa9c7c0f36a7eb1344d6b671ac26
Frank A. Krueger <fak@praeclarum.org>
2022-07-20 14:10:51 -0700

    Put object type lookup in cdli

--- tools/cdli.py
+++ tools/cdli.py
@@ -96,6 +96,59 @@ def get_catalog():
     cat_csv = str(requests.get(cat_url).content, "utf8")
     cat = pd.read_csv(io.StringIO(cat_csv))
     return cat
+
+def merge_atf_with_catalog(atf, cat, tqdm=lambda x: x):
+    output_pubs = []
+    for pub in tqdm(atf):
+        pid = pub.id
+        if len(pid) < 3 or pid[0] != "P":
+            continue
+        if pid[-1] == "=":
+            pid = pid[:-1]
+        pid = int(pid[1:])
+        meta = cat[cat["id_text"]==pid]
+        if len(meta) != 1:
+            continue
+        pub.id = pid
+        pub.genre = str(meta["genre"].values[0])    
+        pub.object_type = str(meta["object_type"].values[0])
+        pub.material = str(meta["material"].values[0])
+        pub.period = str(meta["period"].values[0])
+        pub.provenience = str(meta["provenience"].values[0])
+        pub.collection = str(meta["collection"].values[0])
+        output_pubs.append(pub)
+    output_pubs.sort(key=lambda a: a.id)
+    return output_pubs
+
+object_types = {
+    'brick',
+    'barrel',
+    'cylinder',
+    'cone',
+    'tablet',
+    'seal',
+    'bulla',
+    'vessel',
+    'lentil',
+    'envelope',
+    'block',
+    'prism',
+    'vase',
+    'docket',
+    'sealing',
+    'tag'
+}
+
+def sanitize_object_type(x):
+    return str(x).lower().replace(" (see object remarks)","").replace(" (not impression)", "").replace(" & envelope", "").replace(" in envelope", "").replace("prismatic cylinder", "prism").strip()
+
+def get_object_type(x):
+    x = sanitize_object_type(x)
+    if x == "other":
+        return "other-object"
+    if x in object_types:
+        return x
+    return "other-object"
     
 genres = {
     'administrative',
@@ -103,12 +156,12 @@ genres = {
     'omens',
     'astronomical',
     'school',
-    'prayer/incantation',
+    'prayer-incantation',
     'administative',
     'lexical',
     'pottery',
     'letter',
-    'royal/votive',
+    'royal-votive',
     'uncertain',
     'mathematical',
     'scientific',
@@ -119,14 +172,14 @@ genres = {
     'medical',
     'historical',
     'fake',
-    'royal/monumental',
+    'royal-monumental',
     'administrative record',
     'omen',
-    'private/votive'
+    'private-votive'
 }
 
 def get_genre(x):
-    g = str(x).replace("?","").replace("(see subgenre)","").replace("(modern)", "").replace("(seal)", "").strip().lower()
+    g = str(x).lower().replace("?","").replace("(see subgenre)","").replace("(modern)", "").replace("(seal)", "").replace("/", "-").strip()
     if g == "other":
         return "other-genre"
     if g in genres:
