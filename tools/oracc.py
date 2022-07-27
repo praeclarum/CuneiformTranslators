c4ca276302f15ea3d3e467c031f32232787c0096
Frank A. Krueger <fak@praeclarum.org>
2022-07-27 16:50:29 -0700

    Add easy functions to get all oracc projects

--- tools/oracc.py
+++ tools/oracc.py
@@ -11,6 +11,8 @@ def get_project_ids():
 
 CHUNK_SIZE = 16 * 1024
 
+ignore_project_ids = {'contrib/amarna', 'rinap/rinap5p1', 'cdli', 'cams/tlab', 'cams', 'arrim', 'cams/akno', 'cams/etana', 'cams/barutu', 'amgg', 'lovelyrics', 'ctij', 'lacost', 'contrib', 'cams/selbi', 'cams/ludlul', 'contrib/lambert', 'akklove', 'cams/anzu', 'pnao', 'rimanum'}
+
 def download_project_zip(project_id, out_dir, verbose=True):
     
     requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -26,6 +28,7 @@ def download_project_zip(project_id, out_dir, verbose=True):
     os.makedirs(tmp_dir, exist_ok=True)
     tfile = f"{tmp_dir}/{project_zip_name}"
     response = session.get(url, verify=False)
+    new_ignores = set()
     if response.status_code == 200:
         if verbose:
             print("Downloading " + url)
@@ -37,3 +40,22 @@ def download_project_zip(project_id, out_dir, verbose=True):
     else:
         print(url + " does not exist.")
         return None
+
+def get_all_project_zips(out_dir, verbose=False, tqdm=lambda x: x):
+    project_ids = get_project_ids()
+    paths = []
+    new_ignores = set()
+    for p in tqdm(project_ids):
+        if p in ignore_project_ids:
+            continue
+        path = download_project_zip(p, out_dir, verbose=verbose)
+        if path is not None:
+            paths.append(path)
+        else:
+            new_ignores.add(p)
+    tmp_dir = f"{out_dir}/tmp"
+    if os.path.exists(tmp_dir):
+        os.rmdir(tmp_dir)
+    if len(new_ignores) > 0:
+        print("ignore_project_ids =", repr(new_ignores))
+    return paths
